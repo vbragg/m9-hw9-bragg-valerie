992e7282a9fd7b831cca6271cd71b81077edc58f
Valerie Bragg <vbragg@ufledu>
2019-07-16 14:54:40 -0400

    Add JavaScript functionality to create ES6 classes

---
+++ main.js
@@ -0,0 +1,22 @@
+class Desserts {
+  constructor(type, calories) {
+    this.type = type;
+    this.calories = calories;
+  }
+}
+
+class Icecream extends Desserts {
+  constructor(type, calories, flavor, scoops) {
+    super(type, calories);
+    this.flavor = flavor;
+    this.scoops = scoops;
+  }
+  includeSpoon() {
+    console.log('Here is your spoon!');
+  }
+}
+
+const vanillaIcecream = new Icecream('Icecream', 340, 'Vanilla', 3);
+
+console.log(vanillaIcecream);
+includeSpoon();
